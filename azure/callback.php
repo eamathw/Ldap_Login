731c9e579963bfaf0d9184683fe863705bcf3788
Kipjr <12066560+Kipjr@users.noreply.github.com>
2023-06-05 09:09:04 +0200

    Update classess in callback.php

--- azure/callback.php
+++ azure/callback.php
@@ -7,20 +7,16 @@ use GuzzleHttp\Psr7\Request;
 use Firebase\JWT\JWK;
 use Firebase\JWT\JWT;
 
-$ldap = new Ldap();
-$ldap->load_config();        
-$ldap->write_log("[function]> azure_login");
-
-
-define('TENANT_ID', $ldap->config['ld_azure_tenant_id']);
-define('CLIENT_ID', $ldap->config['ld_azure_client_id']);
-define('CLIENT_SECRET', $ldap->config['ld_azure_client_secret']);
-define('REDIRECT_URI', $ldap->config['ld_azure_redirect_uri']);
-$jwks_url = str_replace("{TENANT_ID}",TENANT_ID,$ldap->config['ld_azure_jwks_url']);
-
 if (isset($_GET['code'])) {
+    global $ld_config,$ld_log;
+    define('TENANT_ID', $ld_config->getValue('ld_azure_tenant_id'));
+    define('CLIENT_ID', $ld_config->getValue('ld_azure_client_id'));
+    define('CLIENT_SECRET', $ld_config->getValue('ld_azure_client_secret'));
+    define('REDIRECT_URI', $ld_config->getValue('ld_azure_redirect_uri'));
+    $jwks_url = str_replace("{TENANT_ID}",TENANT_ID,$ld_config->getValue('ld_azure_jwks_url'));
+    $ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> azure_login");
     $client = new Client();
-    $token_url = str_replace("{TENANT_ID}",TENANT_ID,$ldap->config['ld_azure_token_url']);
+    $token_url = str_replace("{TENANT_ID}",TENANT_ID,$ld_config->getValue('ld_azure_token_url'));
     try{
         $tokenResponse = $client->post($token_url, [
             'form_params' => [
@@ -50,50 +46,49 @@ if (isset($_GET['code'])) {
         try {
             $azureKeys = json_decode(file_get_contents($jwks_url),true);
             $decodedIdToken = JWT::decode($idToken, JWK::parseKeySet($azureKeys,'RS256')); //works
-            $userResource['claim']=$decodedIdToken->{$ldap->config['ld_azure_claim_name']} ?? array();
-            //$userResource->claim=$decodedIdToken[$ldap->config['ldap_azure_claim_name']] ?? array();
+            $userResource['claim']=$decodedIdToken->{$ld_config->getValue('ld_azure_claim_name')} ?? array();
         } catch (\Exception $e) {
             echo 'Exception catched: ',  $e->getMessage(), "\n";
         }
-        $userIdentifier=$ldap->config['ld_azure_user_identifier'];
+        $userIdentifier=$ld_config->getValue('ld_azure_user_identifier');
         $resourceClient = new Client();
         $headers = [
             'Authorization' => 'Bearer ' . $accessToken
         ];
-        $request= new Request('GET', $ldap->config['ld_azure_resource_url'], $headers);          
+        $request= new Request('GET', $ld_config->getValue('ld_azure_resource_url'), $headers);          
         $userResource['data'] = json_decode($resourceClient->sendAsync($request)->wait()->getBody(),true);
         $_SESSION['access_token'] = $accessToken;
         $_SESSION['userObject'] = $userResource;
         //echo("<pre>");print_r($userResource);
 
-		global $prefixeTable;
+		global $prefixeTable,$conf;
 		// search user in piwigo database based on username & additional search on email
 		$query = 'SELECT '.$conf['user_fields']['id'].' AS id FROM '.USERS_TABLE.' WHERE '.$conf['user_fields']['username'].' = \''.pwg_db_real_escape_string($userResource['data'][$userIdentifier]).'\' OR '.$conf['user_fields']['email'].' = \''.pwg_db_real_escape_string($userResource['data']['mail']).'\' ;';
 		$row = pwg_db_fetch_assoc(pwg_query($query));
-		$ldap->write_log("[azure_login]> username found in db:" . (!empty($row1['id'])) . " mail found in db: " . (!empty($row['id'])));
+		$ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> username found in db:" . (!empty($row1['id'])) . " mail found in db: " . (!empty($row['id'])));
 		// if query is not empty, it means everything is ok and we can continue, auth is done !
 		if (!empty($row['id'])) {
 		//user exist
-            if($ldap->config['ld_group_user_active'] == 1) {
+            if($ld_config->getValue('ld_group_user_active') == 1) {
                 $status=false;
             } else {
                 $status='normal';
             }
-            if (in_array($ldap->config['ld_group_user'],  $userResource['claim'])) {
+            if (in_array($ld_config->getValue('ld_group_user'),  $userResource['claim'])) {
                 $status='normal';
             }
-            if (($ldap->config['ld_group_admin_active'] == 1) && (in_array($ldap->config['ld_group_admin'],  $userResource['claim']))) {
+            if (($ld_config->getValue('ld_group_admin_active') == 1) && (in_array($ld_config->getValue('ld_group_admin'),  $userResource['claim']))) {
                 $status='admin';
             }
-            if (($ldap->config['ld_group_webmaster_active'] == 1) && (in_array($ldap->config['ld_group_webmaster'],  $userResource['claim']))) {
+            if (($ld_config->getValue('ld_group_webmaster_active') == 1) && (in_array($ld_config->getValue('ld_group_webmaster'),  $userResource['claim']))) {
                 $status='webmaster';
             }
             if($status == false){
                 trigger_notify('login_failure', stripslashes($userResource['data'][$userIdentifier]));
-                $ldap->write_log("[azure_login]> User does not have role / claim as user to login");
+                $ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> User does not have role / claim as user to login");
                 return false;
             }
-            $ldap->write_log("[azure_login]> Update username in db based on return values of OAuth2 & userIdentifier");                                
+            $ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> Update username in db based on return values of OAuth2 & userIdentifier");                                
             $query = 'UPDATE `'.USERS_TABLE.'` SET `username` =  \''.pwg_db_real_escape_string($userResource['data'][$userIdentifier]).'\' WHERE `'.USERS_TABLE.'`.`id` = ' . $row['id'] . ';';
             pwg_query($query);
             $query = 'UPDATE `'.USER_INFOS_TABLE.'` SET `status` = "'. $status . '" WHERE `'.USER_INFOS_TABLE.'`.`user_id` = ' . $row['id'] . ';';
@@ -104,12 +99,12 @@ if (isset($_GET['code'])) {
 			return true;
 		} else {
 		//user doest not (yet) exist
-			$ldap->write_log("[azure_login]> User found in Azure but not in SQL");
+			$ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> User found in Azure but not in SQL");
 			//this is where we check we are allowed to create new users upon that.
-			if ($ldap->config['ld_allow_newusers']) {
-				$ldap->write_log("[login]> Creating new user and store in SQL");
+			if ($ld_config->getValue('ld_allow_newusers')) {
+				$ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> Creating new user and store in SQL");
 				$mail=null;
-				if($ldap->config['ld_use_mail']){
+				if($ld_config->getValue('ld_use_mail')){
 					//retrieve LDAP e-mail address and create a new user
 					$mail = $userResource['data']['mail'];
 				}
@@ -117,27 +112,27 @@ if (isset($_GET['code'])) {
 				$new_id = register_user($userResource['data'][$userIdentifier],random_password(32),$userResource['data']['mail'],true,$errors);
 				if(count($errors) > 0) {
 					foreach ($errors as &$e){
-						$ldap->write_log("[azure_login]> ".$e, 'ERROR');
+						$ld_log->debug("[".basename(__FILE__)."/".__FUNCTION__."]> ".$e, 'ERROR');
 					}
 					return false;
 				}
-                if($ldap->config['ld_group_user_active'] == 1) {
+                if($ld_config->getValue('ld_group_user_active') == 1) {
                     $status=false;
                 } else {
                     $status='normal';
                 }
-                if (in_array($ldap->config['ld_group_user'],  $userResource['claim'])) {
+                if (in_array($ld_config->getValue('ld_group_user'),  $userResource['claim'])) {
                     $status='normal';
                 }
-                if (($ldap->config['ld_group_admin_active'] == 1) && (in_array($ldap->config['ld_group_admin'],  $userResource['claim']))) {
+                if (( $ld_config->getValue('ld_group_admin_active') == 1 ) && ((in_array($ld_config->getValue('ld_group_admin'),  $userResource['claim'])))) {
                     $status='admin';
                 }
-                if (($ldap->config['ld_group_webmaster_active'] == 1) && (in_array($ldap->config['ld_group_webmaster'],  $userResource['claim']))) {
+                if (( $ld_config->getValue('ld_group_webmaster_active') == 1) && ((in_array($ld_config->getValue('ld_group_webmaster'),  $userResource['claim'])))) {
                     $status='webmaster';
                 }
                 if($status == false){
                     trigger_notify('login_failure', stripslashes($userResource['data'][$userIdentifier]));
-                    $ldap->write_log("[azure_login]> User does not have role / claim as user to login");
+                    $ld_log->error("[".basename(__FILE__)."/".__FUNCTION__."]> User does not have role / claim as user to login");
                     return false;
                 }                              
                 $query = 'UPDATE `'.USER_INFOS_TABLE.'` SET `status` = "'. $status . '" WHERE `'.USER_INFOS_TABLE.'`.`user_id` = ' . $new_id . ';';
@@ -148,7 +143,7 @@ if (isset($_GET['code'])) {
 				trigger_notify('login_success', stripslashes($userResource['data'][$userIdentifier]));
 
 				//in case the e-mail address is empty, redirect to profile page
-				if ($ldap->config['ld_allow_profile']) {
+				if ($ld_config->getValue('ld_allow_profile')) {
 					redirect('profile.php');
 				} else {
 		//                    redirect('index.php');
@@ -159,7 +154,7 @@ if (isset($_GET['code'])) {
 			//else : this is the normal behavior ! user is not created.
 			else {
 				trigger_notify('login_failure', stripslashes($userResource['data'][$userIdentifier]));
-				$ldap->write_log("[azure_login]> Not allowed to create user (ld_allow_newusers=false)");
+				$ld_log->error("[".basename(__FILE__)."/".__FUNCTION__."]> Not allowed to create user (ld_allow_newusers=false)");
 				return false;
 			}
 		}		
@@ -173,7 +168,7 @@ if (isset($_GET['code'])) {
             $error_description = $responseBody->error_description;
             // authorization_pending means we should keep polling
             if (strcmp($error, 'authorization_pending') != 0) {
-                $ldap->write_log('Token endpoint returned: ' . $error . ' ' . $error_description);
+                $ld_log->error("[".basename(__FILE__)."/".__FUNCTION__."]>Token endpoint returned: " . $error . " " . $error_description);
                 return false;
             }
         }
