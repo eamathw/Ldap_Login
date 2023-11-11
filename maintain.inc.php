6a0ceaef38dd1512fdad254c9704899a0e16e9ab
Kipjr <12066560+Kipjr@users.noreply.github.com>
2023-11-11 17:23:13 +0100

    Fix logging

--- maintain.inc.php
+++ maintain.inc.php
@@ -80,39 +80,39 @@ class Ldap_Login_maintain extends PluginMaintain
         if (! ld_table_exist()) { // new install or from old situation
             $ld_config->loadDefaultConfig();
             // prepare sql-table
-            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Created SQL-table');
+            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Created SQL-table');
             ld_sql('create', 'create_table');
-            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Created SQL-data from default values');
+            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Created SQL-data from default values');
             ld_sql('create', 'create_data', $ld_config->getAllValues($default = true));
 
             // everyone, in old situation (ONCE)
             if (file_exists(LDAP_LOGIN_PATH . '/data.dat') && ! file_exists(LDAP_LOGIN_PATH . '/config/data.dat')) { // only in root not in .config/
                 rename(LDAP_LOGIN_PATH . '/data.dat', LDAP_LOGIN_PATH . '/config/data.dat'); // migrate old location to new
-                $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Moved data.dat');
+                $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Moved data.dat');
             }
 
             // future, in new situation (inactivated plugin)
             if (file_exists(LDAP_LOGIN_PATH . '/config/data.dat')) {
-                $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> loading ./config/data.dat ');
-                $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> function load_old_config ');
+                $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> loading ./config/data.dat ');
+                $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> function load_old_config ');
                 $ld_config->loadOldConfig(); // will overwrite default values
                 unlink(LDAP_LOGIN_PATH . '/config/data.dat'); // delete data.dat
-                $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> deleted ./config/data.dat ');
+                $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> deleted ./config/data.dat ');
             }
         }
         else {
             $ld_config->loadDefaultConfig();
-            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Default config loaded ');
+            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Default config loaded ');
             $ld_config->loadConfig($merge = true);
-            $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Merged old config');
+            $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Merged old config');
             ld_sql('create', 'create_data', $ld_config->getAllValues());
-            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Expanded database');
+            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Expanded database');
             ld_sql('update', 'update_sql_structure');
-            $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Added Column with timestamps');
+            $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Added Column with timestamps');
         }
-        $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Saving config');
+        $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Saving config');
         $ld_config->saveConfig();
-        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> plugin installed');
+        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> plugin installed');
         $this->installed = true;
         unset($ld_config, $ld_log);
     }
@@ -141,7 +141,7 @@ class Ldap_Login_maintain extends PluginMaintain
 
         $ld_config->loadDefaultConfig();
         $ld_config->loadConfig($merge = true);
-        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> activate');
+        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> activate');
 
         if (($ld_config->getValue('ld_debug_clearupdate') == 1) or ($ld_config->getValue('ld_debug_clearupdate') == true)) {
             $full = "\n";
@@ -151,18 +151,18 @@ class Ldap_Login_maintain extends PluginMaintain
                 file_put_contents(LDAP_LOGIN_PATH . 'logs/ldap_login.log', $full . "\n");
             }
             else {
-                $ld_log->fatal('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']>Unable to write to ' . LDAP_LOGIN_PATH . 'logs/ldap_login.log');
+                $ld_log->fatal('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']>Unable to write to ' . LDAP_LOGIN_PATH . 'logs/ldap_login.log');
             }
 
-            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> Ldap_login.log cleared');
+            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Ldap_login.log cleared');
         }
 
         if (! $this->installed) {
             // this first after activation.
 
-            $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> [Maintain.inc.php/Install] ');
+            $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> [Maintain.inc.php/Install] ');
             $this->install($plugin_version, $errors); // then install
-            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> plugin activated');
+            $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> plugin activated');
         }
         unset($ld_config, $ld_log);
     }
@@ -203,8 +203,8 @@ class Ldap_Login_maintain extends PluginMaintain
         array_push($handlerArray, $handler); // To php_error.log | NOTICE: PHP message: [2023-05-31T19:39:38.832666+00:00] Ldap_Login.DEBUG
         $ld_log->setHandlers($handlerArray);
         $ld_config = new Config();
-        $ld_log->warning('[' . basename(__FILE__) . '/' . __FUNCTION__ . "]> Check value of 'allow_user_registration' as no user is currently able to register.");
-        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> deactivated');
+        $ld_log->warning('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> Check value of "allow_user_registration" as no user is currently able to register.');
+        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> deactivated');
         unset($ld_config, $ld_log);
     }
 
@@ -222,10 +222,10 @@ class Ldap_Login_maintain extends PluginMaintain
         $ld_log->setFormatter(new LineFormatter(null, null, false, true));
         $ld_log->pushHandler(new ErrorLogHandler(level: Level::Debug)); // To php_error.log | NOTICE: PHP message: [2023-05-31T19:39:38.832666+00:00] Ldap_Login.DEBUG
         $ld_config = new Config();
-        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> uninstall');
+        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> uninstall');
         ld_sql('delete', 'delete_table');
-        $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> removed piwigo_ldap_login_config table');
-        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ']> plugin uninstalled');
+        $ld_log->debug('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> removed piwigo_ldap_login_config table');
+        $ld_log->info('[' . basename(__FILE__) . '/' . __FUNCTION__ . ':' . __LINE__ . ']> plugin uninstalled');
         unset($ld_config, $ld_log);
     }
 
