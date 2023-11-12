050533cccee439988cb12bf49203901897c4d32a
GitHub Action <action@github.com>
2023-11-12 21:26:38 +0000

    Add ./Vendor changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'kipjr/ldap_login',
         'pretty_version' => 'dev-dev',
         'version' => 'dev-dev',
-        'reference' => 'cf45e79733ea5571b0f4161241f384465ddec284',
+        'reference' => 'bda65fd4f004cc61813a5fd2a128f589bbdbaf00',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -49,7 +49,7 @@
         'kipjr/ldap_login' => array(
             'pretty_version' => 'dev-dev',
             'version' => 'dev-dev',
-            'reference' => 'cf45e79733ea5571b0f4161241f384465ddec284',
+            'reference' => 'bda65fd4f004cc61813a5fd2a128f589bbdbaf00',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
